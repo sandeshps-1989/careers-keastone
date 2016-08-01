e72e735b283c87a6d1471d22ca07f94009daeb4e
Sandesh Subramanya <sandeshsubramanya@KSLs-MacBook-Pro.local>
2016-08-01 13:18:48 +0530

    Removed livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,28 +72,28 @@ module.exports = function (grunt) {
       options: {
         port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
-        hostname: 'localhost',
-        livereload: 35729
+        hostname: '0.0.0.0'
+        // livereload: 35729
       },
-      livereload: {
-        options: {
-          open: true,
-          middleware: function (connect) {
-            return [
-              connect.static('.tmp'),
-              connect().use(
-                '/bower_components',
-                connect.static('./bower_components')
-              ),
-              connect().use(
-                '/app/styles',
-                connect.static('./app/styles')
-              ),
-              connect.static(appConfig.app)
-            ];
-          }
-        }
-      },
+      // livereload: {
+      //   options: {
+      //     open: true,
+      //     middleware: function (connect) {
+      //       return [
+      //         connect.static('.tmp'),
+      //         connect().use(
+      //           '/bower_components',
+      //           connect.static('./bower_components')
+      //         ),
+      //         connect().use(
+      //           '/app/styles',
+      //           connect.static('./app/styles')
+      //         ),
+      //         connect.static(appConfig.app)
+      //       ];
+      //     }
+      //   }
+      // },
       test: {
         options: {
           port: 9001,
